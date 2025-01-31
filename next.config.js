031eff374a4249bd442f827fe6e4da23ae9f7eff
Nick-X-Mar <47928530+Nick-X-Mar@users.noreply.github.com>
2025-01-31 10:57:09 +0200

    next config

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  env: {
+    DB_HOST: process.env.DB_HOST,
+    DB_PORT: process.env.DB_PORT,
+    DB_NAME: process.env.DB_NAME,
+    DB_USER: process.env.DB_USER,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    NODE_ENV: process.env.NODE_ENV
+  }
+}
+
+module.exports = nextConfig 
