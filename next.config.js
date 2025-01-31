f0c539fdf1e29303b4410d5ef966e40729d790b1
Nick-X-Mar <47928530+Nick-X-Mar@users.noreply.github.com>
2025-01-31 12:59:27 +0200

    db

--- next.config.js
+++ next.config.js
@@ -5,8 +5,7 @@ const nextConfig = {
     DB_PORT: process.env.DB_PORT,
     DB_NAME: process.env.DB_NAME,
     DB_USER: process.env.DB_USER,
-    DB_PASSWORD: process.env.DB_PASSWORD,
-    NODE_ENV: process.env.NODE_ENV
+    DB_PASSWORD: process.env.DB_PASSWORD
   }
 }
 
