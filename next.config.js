2657162314f35455670f575456aa8ff8fd7f1b4f
Nick-X-Mar <47928530+Nick-X-Mar@users.noreply.github.com>
2025-02-03 20:40:12 +0200

    S3

--- next.config.js
+++ next.config.js
@@ -5,8 +5,10 @@ const nextConfig = {
     DB_PORT: process.env.DB_PORT,
     DB_NAME: process.env.DB_NAME,
     DB_USER: process.env.DB_USER,
-    DB_PASSWORD: process.env.DB_PASSWORD
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    NEXT_PUBLIC_AWS_REGION: 'eu-central-1',
+    NEXT_PUBLIC_AWS_BUCKET_NAME: process.env.AWS_BUCKET_NAME
   }
 }
 
-module.exports = nextConfig 
+module.exports = nextConfig
